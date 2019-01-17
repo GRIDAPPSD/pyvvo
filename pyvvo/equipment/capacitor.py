e32a2d1862903a69e6aae1ce7608d00eb23a6b60
Brandon Thayer <brandon.thayer@pnnl.gov>
2019-01-17 11:19:22 -0600

    - Adding package for power systems equipment object definitions. - Simple cut at adding a SinglePhaseCapacitor: just initialization for now. - SinglePhaseCapacitor initialization tests added.

---
+++ pyvvo/equipment/capacitor.py
@@ -0,0 +1,148 @@
+"""Module for all things capacitors."""
+from pyvvo.utils import list_to_string
+
+
+# class CapacitorMultiPhase:
+#     """Multi-phase capacitor - collection of single phase capacitors.
+#     TODO: Add more information.
+#     """
+#
+#     def __init__(self, name, mrid, phases):
+#         """Initialize capacitor object."""
+#         self.name = name
+#         self.mrid = mrid
+#         self.phases = phases
+#
+
+class CapacitorSinglePhase:
+    """Single phase capacitor.
+    Parameters will for the most part come straight out of the CIM.
+
+    TODO: Accept all CIM parameters.
+    """
+    # Allowed phases (case insensitive)
+    PHASES = ('A', 'B', 'C')
+
+    # Allowed states (case insensitive)
+    STATES = ('OPEN', 'CLOSED')
+
+    def __init__(self, name, mrid, phase, state, name_prefix='cap_'):
+        """Initialize single phase capacitor."""
+        # Assign the name prefix.
+        self.name_prefix = name_prefix
+        # Assign the name. NOTE: the name_prefix will be prepended to
+        # the given name. The syntax below is slightly unintuitive.
+        self.name = name
+
+        # Assign remaining properties.
+        self.mrid = mrid
+        self.phase = phase
+        self.state = state
+
+    ####################################################################
+    # PROPERTY GETTERS AND SETTERS
+    ####################################################################
+
+    @property
+    def name_prefix(self):
+        """Prefix prepended to the given name."""
+        return self._name_prefix
+
+    @name_prefix.setter
+    def name_prefix(self, name_prefix):
+        """Simply set the name_prefix. Must be a string."""
+        if not isinstance(name_prefix, str):
+            raise TypeError('name_prefix must be a string.')
+
+        self._name_prefix = name_prefix
+
+    @property
+    def name(self):
+        """Capacitor name corresponding to object in GridLAB-D model.
+
+        Note that the name_prefix is prepended to the given name.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        """Add prefix to given name, set 'name' attribute."""
+        # Strings only.
+        if not isinstance(name, str):
+            raise TypeError('name must be a string.')
+
+        # Add the name_prefix to the name.
+        self._name = self.name_prefix + name
+
+    @property
+    def mrid(self):
+        """CIM MRID for capacitor."""
+        return self._mrid
+
+    @mrid.setter
+    def mrid(self, mrid):
+        """mrid must be a string.
+        TODO: Enforce valid uuid?
+        """
+        if not isinstance(mrid, str):
+            raise TypeError('mrid must be a string.')
+
+        # Assign.
+        self._mrid = mrid
+
+    @property
+    def phase(self):
+        """Phase which capacitor is on."""
+        return self._phase
+
+    @phase.setter
+    def phase(self, phase):
+        """Phase must be a string, and must be in PHASES.
+
+         Phase is case insensitive.
+         """
+        if not isinstance(phase, str):
+            raise TypeError('phase must be a string.')
+
+        # Cast to upper case.
+        upper_phase = phase.upper()
+
+        # Ensure it's valid.
+        if upper_phase not in self.PHASES:
+            phase_str = list_to_string(self.PHASES, 'or')
+            m = 'phase must be {} (case insensitive).'.format(phase_str)
+            raise ValueError(m)
+
+        # Assign.
+        self._phase = upper_phase
+
+    @property
+    def state(self):
+        """State capacitor is in."""
+        return self._state
+
+    @state.setter
+    def state(self, state):
+        """State must be a string, and must be in STATES.
+
+         State is case insensitive.
+         """
+        if not isinstance(state, str):
+            raise TypeError('state must be a string.')
+
+        # Cast to upper case.
+        upper_state = state.upper()
+
+        # Ensure it's valid.
+        if upper_state not in self.STATES:
+            state_str = list_to_string(self.STATES, 'or')
+            m = 'state must be {} (case insensitive).'.format(state_str)
+            raise ValueError(m)
+
+        # Assign.
+        self._state = upper_state
+
+
+
+
+
---
+++ tests/test_capacitor.py
@@ -0,0 +1,73 @@
+import unittest
+from pyvvo.equipment import capacitor
+
+
+class CapacitorSinglePhaseTestCase(unittest.TestCase):
+    """Basic property tests for CapacitorSinglePhase."""
+
+    def setUp(self):
+        """Create CapacitorSinglePhase object."""
+        self.cap = \
+            capacitor.CapacitorSinglePhase(name='cap1', mrid='1', phase='c',
+                                           state='OpEn', name_prefix='cap_')
+
+    def test_name_prefix(self):
+        self.assertEqual('cap_', self.cap.name_prefix)
+
+    def test_name(self):
+        """Ensure the prefix was added to the name."""
+        self.assertEqual(self.cap.name, 'cap_cap1')
+
+    def test_mrid(self):
+        self.assertEqual('1', self.cap.mrid)
+
+    def test_phase(self):
+        """Lower case phase should be cast to upper case."""
+        self.assertEqual('C', self.cap.phase)
+
+    def test_state(self):
+        """State should be cast to upper case."""
+        self.assertEqual('OPEN', self.cap.state)
+
+
+class CapacitorSinglePhaseBadInputsTestCase(unittest.TestCase):
+    """Test bad inputs to CapacitorSinglePhase"""
+
+    def test_name_prefix_bad_type(self):
+        self.assertRaises(TypeError, capacitor.CapacitorSinglePhase,
+                          name='cap', mrid='1', phase='A', state='OPEN',
+                          name_prefix=1)
+
+    def test_name_bad_type(self):
+        self.assertRaises(TypeError, capacitor.CapacitorSinglePhase,
+                          name=[1, 2, 3], mrid='1', phase='A', state='OPEN',
+                          name_prefix='blah')
+
+    def test_mrid_bad_type(self):
+        self.assertRaises(TypeError, capacitor.CapacitorSinglePhase,
+                          name='1', mrid={'a': 1}, phase='A', state='OPEN',
+                          name_prefix='blah')
+
+    def test_phase_bad_type(self):
+        self.assertRaises(TypeError, capacitor.CapacitorSinglePhase,
+                          name='1', mrid='1', phase=7, state='OPEN',
+                          name_prefix='blah')
+
+    def test_phase_bad_value(self):
+        self.assertRaises(ValueError, capacitor.CapacitorSinglePhase,
+                          name='1', mrid='1', phase='N', state='OPEN',
+                          name_prefix='blah')
+
+    def test_state_bad_type(self):
+        self.assertRaises(TypeError, capacitor.CapacitorSinglePhase,
+                          name='1', mrid='1', phase='c', state=True,
+                          name_prefix='blah')
+
+    def test_state_bad_value(self):
+        self.assertRaises(ValueError, capacitor.CapacitorSinglePhase,
+                          name='1', mrid='1', phase='b', state='stuck',
+                          name_prefix='blah')
+
+
+if __name__ == '__main__':
+    unittest.main()
