34b8ebd69a31a132cb129b299defaa027e23eca2
Brandon Thayer <brandon.thayer@pnnl.gov>
2019-01-24 10:26:47 -0600

    - First crack at creating module and class for performing SPARQL queries - sparql.py     Currently, has basic query functionality and can get all capacitor data. - Accompanying test, test_sparql.py written     Depends on new file, query_capacitors.json - Ran all tests successfully, timestamp in test_zip_1.csv updated.

---
+++ pyvvo/sparql.py
@@ -0,0 +1,229 @@
+"""Module for querying and parsing SPARQL through GridAPPS-D"""
+import logging
+from pyvvo.platform import get_gad_object
+
+
+class SPARQLManager:
+    """Class for querying and parsing SPARQL in GridAPPS-D.
+
+    NOTE: While it's usually conventional to have class constants
+    defined near the beginning of the class definition, we'll be
+    defining them at the bottom since the SPARQL queries are huge.
+    """
+
+    def __init__(self, platform, feeder_mrid, timeout=30):
+        """Connect to the platform.
+
+        :param platform: see input to get_gad_address. '0' or '1'
+        :param feeder_mrid: unique identifier for the feeder in
+            question. Since pyvvo works on a per feeder basis, this is
+            required, and all queries will be executed for the specified
+            feeder.
+        """
+        # Configure log.
+        self.log = logging.getLogger(__name__)
+
+        # Connect to the platform.
+        self.gad = get_gad_object(platform)
+        self.log.debug('Connected to GridAPPS-D.')
+
+        # Assign feeder mrid.
+        self.feeder_mrid = feeder_mrid
+
+        # Timeout for SPARQL queries.
+        self.timeout=timeout
+
+    ####################################################################
+    # QUERY METHODS
+    ####################################################################
+
+    def query_data(self, query_string):
+        """Wrapper to call GridAPPSD.query_data with error handling."""
+        # Perform query.
+        result = self.gad.query_data(query=query_string, timeout=self.timeout)
+
+        # Check for error (bad syntax, etc.).
+        try:
+            result['error']
+        except KeyError:
+            # No problem.
+            pass
+        else:
+            # The 'error' key exists.
+            m = 'The given query resulted in an error.'
+            e = SPARQLQueryError(query=query_string, message=m)
+            self.log.error('The following SPARQL query resulted in an '
+                           + 'error:\n    {}'.format(query_string))
+            raise e
+
+        # Check for empty query return.
+        if len(result['data']['results']['bindings']) == 0:
+            m = 'The given query did not return any data.'
+            e = SPARQLQueryReturnEmptyError(query=query_string,
+                                            message=m)
+            self.log.error('The following SPARQL query did not return any '
+                           + 'data:\n    {}'.format(e.query))
+            raise e
+
+        # Done.
+        return result
+
+    def query_named_objects(self, query_string):
+        """Helper to perform a data query for named objects.
+
+        NOTE: All queries MUST return an object name. If this is too
+        restrictive it can be modified later.
+        """
+        # Perform query.
+        result = self.query_data(query_string)
+
+        # Get dictionary of objects.
+        output = \
+            self._bindings_to_dict(result['data']['results']['bindings'])
+
+        return output
+
+    def query_capacitors(self):
+        """Get information on capacitors in the feeder."""
+        # Perform the query.
+        result = self.query_named_objects(
+            self.CAPACITOR_QUERY.format(feeder_mrid=self.feeder_mrid))
+        self.log.info('Capacitor data obtained.')
+
+        # Done.
+        return result
+
+    ####################################################################
+    # HELPER FUNCTIONS
+    ####################################################################
+
+    def _bindings_to_dict(self, bindings):
+        """Given a list of bindings, map them into a usable dictionary.
+
+        The dictionary will be keyed by 'name,' so all bindings must
+        have a 'name' attribute. So the upstream SPARQL query should be
+        naming the name variable 'name.'
+        """
+        # Ensure bindings is a list.
+        if not isinstance(bindings, list):
+            self.log.error(("'Bindings' is not a list:"
+                            "\n    {}".format(bindings)))
+            raise TypeError("The bindings input must be a list.")
+
+        # Crude check to see if we have a 'name' attribute - assuming
+        # all elements will have it.
+        try:
+            bindings[0]['name']
+        except KeyError:
+            m = ("Missing 'name' attribute in bindings:\n    "
+                 + "{}".format(bindings))
+            self.log.error(m)
+            raise KeyError("All bindings must have a 'name' attribute.")
+
+        # Loop over the bindings and simplify to dictionary keyed by
+        # object name, and only includes attribute names and values.
+        output = dict()
+        for obj in bindings:
+            # Perform the simplification using dictionary comprehension.
+            # Note we'll get a KeyError if
+            output[obj['name']['value']] = \
+                {k: v['value'] for (k, v) in obj.items()}
+
+        self.log.debug("Bindings mapped into dictionary.")
+        return output
+
+    ####################################################################
+    # SPARQL QUERY TEXT
+    ####################################################################
+
+    # Define query prefix
+    PREFIX = ("PREFIX r: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n"
+              "PREFIX c: <http://iec.ch/TC57/2012/CIM-schema-cim17#>\n"
+              "PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n"
+              )
+
+    # Query for getting capacitor information. Should be formatted with a
+    # # feeder ID: .format(feeder_mrid=feeder_mrid)
+    CAPACITOR_QUERY = \
+        (PREFIX +
+         "SELECT ?name ?basev ?nomu ?bsection ?bus ?conn ?grnd ?phs "
+         "?ctrlenabled ?discrete ?mode ?deadband ?setpoint ?delay "
+         "?monclass ?moneq ?monbus ?monphs ?mrid ?feeder_mrid "
+         "WHERE {{ "
+         "?s r:type c:LinearShuntCompensator. "
+         'VALUES ?feeder_mrid {{"{feeder_mrid}"}} '
+         "?s c:Equipment.EquipmentContainer ?fdr. "
+         "?fdr c:IdentifiedObject.mRID ?feeder_mrid. "
+         "?s c:IdentifiedObject.name ?name. "
+         "?s c:ConductingEquipment.BaseVoltage ?bv. "
+         "?bv c:BaseVoltage.nominalVoltage ?basev. "
+         "?s c:ShuntCompensator.nomU ?nomu. "
+         "?s c:LinearShuntCompensator.bPerSection ?bsection. "
+         "?s c:ShuntCompensator.phaseConnection ?connraw. "
+         'bind(strafter(str(?connraw),"PhaseShuntConnectionKind.")'
+         " as ?conn) "
+         "?s c:ShuntCompensator.grounded ?grnd. "
+         "OPTIONAL {{ "
+         "?scp c:ShuntCompensatorPhase.ShuntCompensator ?s. "
+         "?scp c:ShuntCompensatorPhase.phase ?phsraw. "
+         'bind(strafter(str(?phsraw),"SinglePhaseKind.")'
+         " as ?phs) "
+         "}} "
+         "OPTIONAL {{ "
+         "?ctl c:RegulatingControl.RegulatingCondEq ?s. "
+         "?ctl c:RegulatingControl.discrete ?discrete. "
+         "?ctl c:RegulatingControl.enabled ?ctrlenabled. "
+         "?ctl c:RegulatingControl.mode ?moderaw. "
+         'bind(strafter(str(?moderaw),'
+         '"RegulatingControlModeKind.")'
+         " as ?mode) "
+         "?ctl c:RegulatingControl.monitoredPhase ?monraw. "
+         'bind(strafter(str(?monraw),"PhaseCode.") as ?monphs) '
+         "?ctl c:RegulatingControl.targetDeadband ?deadband. "
+         "?ctl c:RegulatingControl.targetValue ?setpoint. "
+         "?s c:ShuntCompensator.aVRDelay ?delay. "
+         "?ctl c:RegulatingControl.Terminal ?trm. "
+         "?trm c:Terminal.ConductingEquipment ?eq. "
+         "?eq a ?classraw. "
+         'bind(strafter(str(?classraw),"cim17#") as ?monclass) '
+         "?eq c:IdentifiedObject.name ?moneq. "
+         "?trm c:Terminal.ConnectivityNode ?moncn. "
+         "?moncn c:IdentifiedObject.name ?monbus. "
+         "}} "
+         "?s c:IdentifiedObject.mRID ?mrid. "
+         "?t c:Terminal.ConductingEquipment ?s. "
+         "?t c:Terminal.ConnectivityNode ?cn. "
+         "?cn c:IdentifiedObject.name ?bus "
+         "}} "
+         "ORDER by ?name "
+         )
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class SPARQLQueryReturnEmptyError(Error):
+    """Raised if a SPARQL query returns empty bindings.
+
+    Attributes:
+        query -- SPARQL query that resulted in empty return.
+        message -- explanation of the error.
+    """
+    def __init__(self, query, message):
+        self.query = query.replace('\n', '\n    ')
+        self.message = message
+
+
+class SPARQLQueryError(Error):
+    """Raised if a SPARQL query returns an error.
+
+    Attributes:
+        query -- SPARQL query that resulted in empty return.
+        message -- explanation of the error.
+    """
+
+    def __init__(self, query, message):
+        self.query = query.replace('\n', '\n    ')
+        self.message = message
---
+++ tests/test_sparql.py
@@ -0,0 +1,160 @@
+# Standard library.
+import unittest
+from unittest.mock import patch
+import os
+
+# pyvvo.
+from pyvvo import sparql
+
+# Third-party.
+import simplejson as json
+from stomp.exception import ConnectFailedException
+
+# Hard-code 8500 node feeder MRID.
+FEEDER_MRID = '_4F76A5F9-271D-9EB8-5E31-AA362D86F2C3'
+
+
+class SPARQLManagerTestCase(unittest.TestCase):
+    """General tests for the SPARQLManager
+
+    NOTE: Tests will be skipped if platform connection fails.
+
+    TODO: Rather than relying on a connection, we should probably mock
+        everything up. However, connecting to the platform and actually
+        performing queries helps catch regression issues with the
+        platform itself, which is handy.
+    """
+
+    def setUp(self):
+        """Attempt to connect to the platform."""
+        try:
+            platform_var = os.environ['platform']
+        except KeyError:
+            m = ('Environment variable "platform" must be defined to connect '
+                 + 'to the GridAPPS-D platform. With PyCharm, you can use the '
+                 + 'EnvFile plugin to make things easier.')
+            raise KeyError(m)
+
+        try:
+            self.sparql = sparql.SPARQLManager(platform=platform_var,
+                                               feeder_mrid=FEEDER_MRID)
+        except ConnectFailedException:
+            # We cannot connect to the platform.
+            raise unittest.SkipTest('Failed to connect to GridAPPS-D.')
+
+    def test_sparql_manager_is_sparql_manager(self):
+        self.assertIsInstance(self.sparql, sparql.SPARQLManager)
+
+    def test_sparql_manager_bindings_to_dict_valid_input(self):
+        bindings = [
+            {'name': {'type': 'literal', 'value': 'obj1'},
+             'stuff': {'value': '2'}
+             },
+            {'things': {'type': 2, 'value': 17},
+             'name': {'value': 'I am a thing.'}
+             }
+        ]
+
+        expected_value = {'obj1': {'name': 'obj1', 'stuff': '2'},
+                          'I am a thing.': {'things': 17,
+                                            'name': 'I am a thing.'}}
+
+        self.assertEqual(self.sparql._bindings_to_dict(bindings),
+                         expected_value)
+
+    def test_sparql_manager_bindings_to_dict_no_name(self):
+        self.assertRaises(KeyError, self.sparql._bindings_to_dict,
+                          bindings=[{'I have no name': 'agh'}])
+
+    def test_sparql_manager_bindings_to_dict_no_value(self):
+        self.assertRaises(KeyError, self.sparql._bindings_to_dict,
+                          bindings=[{'name': {'type': 'literal'}}])
+
+    def test_sparql_manager_bindings_to_dict_bad_binding_type(self):
+        self.assertRaises(TypeError, self.sparql._bindings_to_dict,
+                          bindings={'name': {'value': 'device',
+                                             'type': 'stuff'}})
+
+    def test_sparql_manager_bindings_to_dict_bad_binding_element_type(self):
+        self.assertRaises(TypeError, self.sparql._bindings_to_dict,
+                          bindings=[{'name': 10}])
+
+    def test_sparql_manager_query_named_objects_simple_query(self):
+        """Hard-coded MRID for 8500 node capacitor."""
+        query = (self.sparql.PREFIX +
+                 "SELECT ?name "
+                 "WHERE { "
+                 '?s c:IdentifiedObject.mRID'
+                 '  "_A5866105-A527-F682-C982-69807C0E088B". '
+                 "?s c:IdentifiedObject.name ?name. "
+                 "} "
+                 "ORDER BY ?name "
+                 )
+        result = self.sparql.query_named_objects(query)
+
+        self.assertDictEqual(result, {'capbank0a': {'name': 'capbank0a'}})
+
+    def test_sparql_manager_query_named_objects_calls_query_data(self):
+        """Ensure query_named_objects calls query_data, as query_data
+        does the error handling.
+        """
+        # Mock the silly-deep return from the platform.
+        config = \
+            {'return_value': {'data': {'results': {'bindings':
+                                                       [{'name': {
+                                                           'value': 'object1'}}]}}}}
+
+        with patch('pyvvo.sparql.SPARQLManager.query_data', **config) as mock:
+            result = self.sparql.query_named_objects('some query')
+            mock.assert_called_once()
+            mock.assert_called_with('some query')
+            self.assertDictEqual(result, {'object1': {'name': 'object1'}})
+
+    def test_sparql_manager_query_data_no_results(self):
+        """Give a SPARQL query which won't return any data."""
+        query = (self.sparql.PREFIX +
+                 "SELECT ?name "
+                 "WHERE { "
+                 '?s c:NotReal ?name. '
+                 "} "
+                 "ORDER BY ?name "
+                 )
+
+        self.assertRaises(sparql.SPARQLQueryReturnEmptyError,
+                          self.sparql.query_data, query_string=query)
+
+    def test_sparql_manager_query_data_bad_syntax(self):
+        """Give SPARQL query with bad syntax."""
+        query = 'This is not actually a query.'
+        self.assertRaises(sparql.SPARQLQueryError, self.sparql.query_data,
+                          query_string=query)
+
+    def test_sparql_manager_query_capacitors(self):
+        """Ensure we get the expected return."""
+        actual = self.sparql.query_capacitors()
+
+        # Uncomment stuff below to re-create the expected output.
+        # with open('query_capacitors.json', 'w') as f:
+        #     json.dump(actual, f)
+
+        # Load expected result.
+        with open('query_capacitors.json', 'r') as f:
+            expected = json.load(f)
+
+        self.assertDictEqual(actual, expected)
+
+    def test_sparql_manager_query_capacitors_calls_query_named_objects(self):
+        """query_capacitors must call query_named_objects."""
+
+        with patch('pyvvo.sparql.SPARQLManager.query_named_objects',
+                   return_value='success') as mock:
+            cap_return = self.sparql.query_capacitors()
+            mock.assert_called_once()
+            mock.assert_called_with(
+                self.sparql.CAPACITOR_QUERY.format(
+                    feeder_mrid=self.sparql.feeder_mrid))
+            self.assertEqual('success', cap_return)
+
+
+if __name__ == '__main__':
+    unittest.main()
