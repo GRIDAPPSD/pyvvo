005db960bf96fd2d91b6e425904f3643cea4a0a7
Brandon Thayer <brandon.thayer@pnnl.gov>
2019-02-14 11:53:42 -0600

    Adding new module, app.py. Eventually this will be the main entrance for     pyvvo. For now, using it for testing/debugging.

---
+++ pyvvo/app.py
@@ -0,0 +1,65 @@
+"""Main module for running the pyvvo application."""
+from pyvvo.sparql import SPARQLManager
+from pyvvo.glm import GLMManager
+from pyvvo.gridappsd_platform import PlatformManager
+from pyvvo.utils import run_gld
+from datetime import datetime
+
+if __name__ == '__main__':
+    # # Determine whether we're running inside or outside the platform.
+    # PLATFORM = os.environ['platform']
+    #
+    # Hard-code 8500 node MRID for now.
+    feeder_mrid = '_4F76A5F9-271D-9EB8-5E31-AA362D86F2C3'
+    # IEEE 13 bus
+    # feeder_mrid = '_49AD8E07-3BF9-A4E2-CB8F-C3722F837B62'
+
+    # Get a SPARQL manager.
+    sparql = SPARQLManager(feeder_mrid=feeder_mrid)
+
+    # Get a platform manager
+    platform = PlatformManager()
+
+    # Hard-code some dates to work with.
+    starttime = datetime(2013, 1, 14)
+    stoptime = datetime(2013, 1, 21)
+
+    ####################################################################
+    # GET PREREQUISITE DATA
+    ####################################################################
+
+    # Get regulator information.
+    regs = sparql.query_regulators()
+    reg_meas = sparql.query_rtc_measurements()
+
+    # Get capacitor information.
+    caps = sparql.query_capacitors()
+    cap_meas = sparql.query_capacitor_measurements()
+
+    # Get EnergyConsumer (load) data.
+    load_nom_v = sparql.query_load_nominal_voltage()
+    load_meas = sparql.query_load_measurements()
+
+    # Get substation data.
+    substation = sparql.query_substation_source()
+    substation_bus_meas = sparql.query_measurements_for_bus(
+        bus_mrid=substation.iloc[0]['bus_mrid'])
+
+    # Get feeder model, initialize GLMManager.
+    model = platform.get_glm(model_id=feeder_mrid)
+    glm = GLMManager(model=model, model_is_path=False)
+
+    # Get historic load measurement data.
+    # TODO.
+
+    # Get historic weather.
+    weather = platform.get_weather(start_time=starttime, end_time=stoptime)
+
+    # Get current weather.
+    # TODO - what's the way forward here? Probably just query the
+    #   database.
+
+    pass
+
+    # # Run a simulation.
+    # sim_id = platform.run_simulation()
