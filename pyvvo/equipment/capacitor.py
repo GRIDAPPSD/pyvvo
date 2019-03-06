dc6708b459fa03490f09d795baeea98d21490ce3
Brandon Thayer <brandon.thayer@pnnl.gov>
2019-03-06 10:17:18 -0600

    Remove setter methods in capacitor.py to make it explicit properties are     private.

--- pyvvo/equipment/capacitor.py
+++ pyvvo/equipment/capacitor.py
@@ -64,84 +64,27 @@ class CapacitorSinglePhase:
         # Get log.
         self.log = logging.getLogger(__name__)
 
-        # Assign the name prefix.
-        self.name_prefix = name_prefix
-        # Assign the name. NOTE: the name_prefix will be prepended to
-        # the given name in the setter method.
-        self.name = name
-
-        # Assign remaining properties.
-        self.mrid = mrid
-        self.phase = phase
-        self.state = state
-
-        self.log.debug('CapacitorSinglePhase {} '.format(self.name)
-                       + 'initialized.')
-
-    def __repr__(self):
-        return self.name
-
-    ####################################################################
-    # PROPERTY GETTERS AND SETTERS
-    ####################################################################
-    @property
-    def name_prefix(self):
-        """Prefix prepended to the given name."""
-        return self._name_prefix
-
-    @name_prefix.setter
-    def name_prefix(self, name_prefix):
-        """Simply set the name_prefix. Must be a string."""
+        # Check and assign the name prefix.
         if not isinstance(name_prefix, str):
             raise TypeError('name_prefix must be a string.')
 
         self._name_prefix = name_prefix
 
-    @property
-    def name(self):
-        """Capacitor name corresponding to object in GridLAB-D model.
-
-        Note that the name_prefix is prepended to the given name.
-        """
-        return self._name
-
-    @name.setter
-    def name(self, name):
-        """Add prefix to given name, set 'name' attribute."""
-        # Strings only.
+        # Check and assign the name. NOTE: the name_prefix will be
+        # prepended to the given name.
         if not isinstance(name, str):
             raise TypeError('name must be a string.')
 
         # Add the name_prefix to the name.
         self._name = self.name_prefix + name
 
-    @property
-    def mrid(self):
-        """CIM MRID for capacitor."""
-        return self._mrid
-
-    @mrid.setter
-    def mrid(self, mrid):
-        """mrid must be a string.
-        TODO: Enforce valid uuid?
-        """
+        # Check and assign MRID.
         if not isinstance(mrid, str):
             raise TypeError('mrid must be a string.')
 
-        # Assign.
         self._mrid = mrid
 
-    @property
-    def phase(self):
-        """Phase which capacitor is on."""
-        return self._phase
-
-    @phase.setter
-    def phase(self, phase):
-        """Phase must be a string, and must be in PHASES.
-
-         Phase is case insensitive.
-         """
+        # Check and assign phase.
         if not isinstance(phase, str):
             raise TypeError('phase must be a string.')
 
@@ -157,21 +100,10 @@ class CapacitorSinglePhase:
         # Assign.
         self._phase = upper_phase
 
-    @property
-    def state(self):
-        """State capacitor is in. OPEN/CLOSED/None. None --> unknown."""
-        return self._state
-
-    @state.setter
-    def state(self, state):
-        """State must be None or a string, and must be in STATES.
-
-         State is case insensitive.
-         """
-        # If state is None, simply set and return.
+        # Check and assign state.
         if state is None:
+            # If state is None, simply set.
             self._state = None
-            return
         elif isinstance(state, str):
             # Cast to upper case.
             upper_state = state.upper()
@@ -182,10 +114,45 @@ class CapacitorSinglePhase:
                 m = 'state must be {} (case insensitive).'.format(state_str)
                 raise ValueError(m)
 
-            # Assign and return.
+            # Assign.
             self._state = upper_state
-            return
         else:
+            # State is a bad type.
             raise TypeError('state must None or be a string.')
 
-        # That's it.
+        self.log.debug('CapacitorSinglePhase {} '.format(self.name)
+                       + 'initialized.')
+
+    def __repr__(self):
+        return self.name
+
+    ####################################################################
+    # PROPERTY GETTERS AND SETTERS
+    ####################################################################
+    @property
+    def name_prefix(self):
+        """Prefix prepended to the given name."""
+        return self._name_prefix
+
+    @property
+    def name(self):
+        """Capacitor name corresponding to object in GridLAB-D model.
+
+        Note that the name_prefix is prepended to the given name.
+        """
+        return self._name
+
+    @property
+    def mrid(self):
+        """CIM MRID for capacitor."""
+        return self._mrid
+
+    @property
+    def phase(self):
+        """Phase which capacitor is on."""
+        return self._phase
+
+    @property
+    def state(self):
+        """State capacitor is in. OPEN/CLOSED/None. None --> unknown."""
+        return self._state
