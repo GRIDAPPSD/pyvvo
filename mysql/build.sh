99a12098b88cd845715282e44ebb8ed1897ea095
Brandon Thayer <brandon.thayer@pnnl.gov>
2019-11-04 17:42:48 -0600

    Changing MySQL container tag so that it can be pushed to the gridappsd/pyvvo Docker repo.

--- mysql/build.sh
+++ mysql/build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Simple helper for building the MySQL image.
-docker build -t gridappsd/pyvvo-mysql:latest .
+docker build -t gridappsd/pyvvo:mysql-latest .
+docker push gridappsd/pyvvo:mysql-latest
