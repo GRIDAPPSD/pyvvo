d362bbbfaa12cbe982d1867c7b84c55d92da9517
Brandon Thayer <brandon.thayer@pnnl.gov>
2020-06-18 15:19:09 -0700

    Updating docs/conf.py to use autodoc and mathjax extensions, and adding some autodoc configuration.

--- docs/conf.py
+++ docs/conf.py
@@ -10,15 +10,17 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+# Add top-level of repository to the path for auto-documentation.
+sys.path.insert(0, os.path.abspath('../'))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'PyVVO'
-copyright = '2020, Brandon Thayer'
+copyright = '2020, Pacific Northwest National Laboratory and Brandon Thayer'
 author = 'Brandon Thayer'
 
 
@@ -27,8 +29,7 @@ author = 'Brandon Thayer'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -50,3 +51,39 @@ html_theme = 'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+########################################################################
+# Prevent skipping __init___
+# Source: https://stackoverflow.com/a/9772922
+########################################################################
+
+autoclass_content = 'both'
+
+########################################################################
+# Save some typing and automagically set some autodoc flags.
+########################################################################
+autodoc_default_options = {'members': True, 'undoc-members': True,
+                           'show-inheritance': True}
+
+########################################################################
+# Mock up imports so that the documentation can be built in a simple
+# virtual environment with only sphinx installed.
+########################################################################
+
+# Note: setup.py specifies mysqlclient, but really we import MySQLdb.
+#
+# Note: gridappsd is not listed in setup.py since it's included in the
+# base Docker image for the application.
+#
+# Note: simplejson is not present here so that __init__.py can actually
+# be run. All modules have been adjusted to fall back to the json
+# package if simplejson cannot be found.
+#
+# Note: scikit-learn imports as sklearn
+#
+# Note: due to some constants in zip.py (and possibly elsewhere)
+# depending on numpy, our documentation build environment should have
+# numpy installed and NOT mocked.
+autodoc_mock_imports = [
+    'MySQLdb', 'mysqlclient', 'pandas', 'python-dateutil',
+    'scipy', 'sklearn', 'stomp', 'gridappsd', 'dateutil']
